ce48461809a230f44b1cfa4e9d993c9189355691
Gihan <gihansujith943@gmail.com>
2024-07-26 15:01:48 +0530

    add-customer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { RegisterComponent } from './customer/register/register.component';
+import { CustomerNavbarComponent } from './customer/customer-navbar/customer-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,RegisterComponent,CustomerNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { CustomerNavbarComponent } from './customer/customer-navbar/customer-navbar.component';
 
 export const routes: Routes = [];
+
